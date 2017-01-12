298a38860cf4d117cab2a7adf61f2e9807c50182
dburnet <darryl.burnet@users.noreply.github.com>
2017-01-12 12:14:47 -0500

    1.12 update
    updates from Winter break 2016

--- me_RestingAsymmetryReadIn.m
+++ me_RestingAsymmetryReadIn.m
@@ -3,7 +3,7 @@ function me_RestingAsymmetryReadIn(files,outfile,filename,sheet,xlRange)
 
 fid = fopen(outfile, 'w');
 %  A = ('Filename;SubjectNumber;SessionNumber;1;2;3;4;5;6;7');
-    fprintf(fid, '%6s, %12s, %18s, %24s, %30s, %36s, %42s, %48s, %54s, %60s, %66,','Filename,SubjectNumber,SessionNumber,1,2,3,4,5,6,7,'',' );
+    fprintf(fid, '%6s, %12s, %18s, %24s, %30s, %36s, %42s, %48s, %54s, %60s, %66\r\n\,','Filename,SubjectNumber,SessionNumber,1,2,3,4,5,6,7,'',' );
     
 for file = files'
     file.name
@@ -16,6 +16,7 @@ for file = files'
     x = me_restingAsym(eegname, filename,sheet,xlRange); 
     fprintf(fid, '%s,%s,%s,%s,', eegname, subnum, sessionnumber,eyestatus);
     fprintf( fid, '%f,%f,%f,%f,%f,%f,%f\n,', x);
+    fprintf(fid, '\n');
 end
 fclose(fid);
 fclose('all');
